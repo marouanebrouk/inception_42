282a3c363f629d380aa2f754ce9076e51d223b0e
marouanebrouk <socialmarouanebrouk@gmail.com>
2026-08-19 21:09:14 +0100

    add entrypoint

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -13,4 +13,16 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     sed -i "s/localhost/${MYSQL_HOST}/" /var/www/html/wp-config.php
 fi
 
+if ! wp core is-installed --allow-root; then
+    echo "Installing WordPress..."
+
+    wp core install \
+        --url="${WP_URL}" \
+        --title="${WP_TITLE}" \
+        --admin_user="${WP_ADMIN_USER}" \
+        --admin_password="${WP_ADMIN_PASSWORD}" \
+        --admin_email="${WP_ADMIN_EMAIL}" \
+        --allow-root
+fi
+
 exec "$@"
